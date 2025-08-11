621ad317bd66dcbba3552ee5d72dac8ab8af10a4
shavouz <tal.kalman3@gmail.com>
2025-08-10 20:10:24 -0400

    new

---
+++ _astro/chevron-right.CGiYgUZ9.js
@@ -0,0 +1,11 @@
+import{a as o}from"./createLucideIcon.eztlw-if.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],h=o("ChevronLeft",e);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],c=o("ChevronRight",t);export{h as C,c as a};
