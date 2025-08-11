a229f6ddab2180b285891a82961bcd836b0bcc86
shavouz <tal.kalman3@gmail.com>
2025-08-11 00:38:37 -0400

    asdfsadf

---
+++ _astro/textarea.BFZwh3TD.js
@@ -0,0 +1,16 @@
+import{a as r,j as a,c as s}from"./createLucideIcon.CVs7V5kV.js";import{r as i}from"./index.DNi1g-pO.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]],x=r("CircleCheckBig",d);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],b=r("LoaderCircle",l);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],g=r("Send",c),p=i.forwardRef(({className:e,type:t,...o},n)=>a.jsx("input",{type:t,className:s("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),ref:n,...o}));p.displayName="Input";const u=i.forwardRef(({className:e,...t},o)=>a.jsx("textarea",{className:s("flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),ref:o,...t}));u.displayName="Textarea";export{x as C,p as I,b as L,g as S,u as T};
