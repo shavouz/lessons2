fdf15a0b39d6964eca292dc7c05ffbf74a087771
shavouz <tal.kalman3@gmail.com>
2025-08-10 23:42:53 -0400

    asfsdfasdf

---
+++ _astro/index.BiGv7wnI.js
@@ -0,0 +1,6 @@
+import{c as r}from"./createLucideIcon.4tpODiV5.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],c=r("ArrowLeft",o);function a(e){return"/"+e.toLowerCase().replace(/ /g,"-")}export{c as A,a as c};
