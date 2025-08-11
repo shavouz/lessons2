705ed71e801d022a1f91703fe1ad5193e537f92d
shavouz <tal.kalman3@gmail.com>
2025-08-11 17:19:14 -0400

    fdsasdffasd

---
+++ _astro/textarea.BqtTyBSs.js
@@ -0,0 +1,6 @@
+import{c as n,j as o,a as s}from"./createLucideIcon.4tpODiV5.js";import{r as a}from"./index.DNi1g-pO.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]],f=n("CircleCheckBig",d),l=a.forwardRef(({className:e,type:r,...t},i)=>o.jsx("input",{type:r,className:s("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),ref:i,...t}));l.displayName="Input";const c=a.forwardRef(({className:e,...r},t)=>o.jsx("textarea",{className:s("flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),ref:t,...r}));c.displayName="Textarea";export{f as C,l as I,c as T};
