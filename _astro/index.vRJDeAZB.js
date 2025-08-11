2183af5243112142d8e6923f1dbc9de7c34cfdfb
shavouz <tal.kalman3@gmail.com>
2025-08-10 22:25:42 -0400

    changes

--- _astro/index.vRJDeAZB.js
+++
@@ -1,6 +0,0 @@
-import{a as r}from"./createLucideIcon.eztlw-if.js";/**
- * @license lucide-react v0.475.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],a=r("ArrowLeft",o);function c(e){return"/"+e.toLowerCase().replace(/ /g,"-")}export{a as A,c};
